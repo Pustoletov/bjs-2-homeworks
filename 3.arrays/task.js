a178153aea4cb681a7bb5d61310b85c42c5ec20f
Siun  Maksim <myrdym@rambler.ru>
2021-07-03 16:17:56 +0300

    all tasks completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
-function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+const compareArrays = (arr1,arr2) => {
+  let arrayOne = arr1;
+  let arrayTwo = arr2;
+  let result = arrayOne.every((elem,indx,arr) => elem === arrayTwo[indx] && arrayOne.length === arrayTwo.length );
   return result; // boolean
 }
-
-function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+ 
+const advancedFilter = (arr) => {
+  let arrayOne = arr;
+  let resultArr = arrayOne.filter(elem => elem % 3 === 0).filter(elem => elem > 0).map(elem =>  elem * 10);
   return resultArr; // array
 }
